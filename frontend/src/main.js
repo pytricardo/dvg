a1d6652c116f1c4b03322635df982fd13e213e5b
pytricardo <rfrailen@yahoo.es>
2022-02-10 10:36:43 +0100

    Primer commit proyecto dvg

---
+++ frontend/src/main.js
@@ -0,0 +1,19 @@
+import router from '@/router'
+
+import Vue from 'vue'
+import App from './App.vue'
+/* import router from './router' */
+import { createProvider } from './vue-apollo'
+
+
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  apolloProvider: createProvider({
+    httpEndpoint: 'http://localhost:8000/graphql',
+    wsEndpoint: null,
+  }),
+  render: h => h(App)
+}).$mount('#app')
